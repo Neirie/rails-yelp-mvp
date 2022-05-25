532ea238a50dec2b10d74f85664a0ccf47375206
Laurence Nguyen <laurence@oneiri.eu>
2022-05-25 17:01:09 +0200

    challenge not completed yet

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'reviews/pages'
+  resources :restaurants
+    resources :reviews, only: [:new, :creat, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
